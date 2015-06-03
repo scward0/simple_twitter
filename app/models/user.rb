823b0b8b76fef3049aacade4d67e7b4dba35a957
Bradley Hintze <Bradley.Hintze@gmail.com>
2015-06-03 15:02:54 -0600

    new users must have unique email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_many :tweets
   has_many :followers
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
